d02aa436411dddf927860e79a5b866a1b51ab28f
youcefchabani1204 <52547918+youcefchabani1204@users.noreply.github.com>
2025-02-07 21:49:21 +0100

    Mise à jour du composant OpportunityProductViewer

--- force-app/main/default/lwc/opportunityProductViewer/opportunityProductViewer.js
+++ force-app/main/default/lwc/opportunityProductViewer/opportunityProductViewer.js
@@ -3,42 +3,69 @@ import  getOpportunityProducts from '@salesforce/apex/OpportunityProductControll
 import CheckIfAdmin from '@salesforce/apex/UserProfile.CheckIfAdmin';
 import deleteProductById from '@salesforce/apex/DeleteItem.deleteProductById';
 import { NavigationMixin } from 'lightning/navigation';
+import PRODUCT_NAME from '@salesforce/label/c.Product_Name';
+import QUANTITY from '@salesforce/label/c.Quantity';
+import UNIT_PRICE from '@salesforce/label/c.Unit_Price';
+import TOTAL_PRICE from '@salesforce/label/c.Total_Price';
+import QUANTITY_IN_STOCK from '@salesforce/label/c.Quantity_In_Stock';
+import DELETE from '@salesforce/label/c.Delete';
+import See_Product from '@salesforce/label/c.See_Product';
+import Quantity_Problem_Message from '@salesforce/label/c.Quantity_Problem_Message';
+import NO_PRODUCT_LINES from '@salesforce/label/c.No_Product_Lines';
+import Opportunity_Products from '@salesforce/label/c.Opportunity_Products';
+import Opportunity_Update_Problem from '@salesforce/label/c.Opportunity_Update_Problem';
+
 
 export default class OpportunityProductViewer extends NavigationMixin(LightningElement) {
+    labels = {
+        productName: PRODUCT_NAME,
+        quantity: QUANTITY,
+        unitPrice: UNIT_PRICE,
+        totalPrice: TOTAL_PRICE,
+        quantityInStock: QUANTITY_IN_STOCK,
+        delete: DELETE,
+         quantityProblemMessage:Quantity_Problem_Message,
+        noProductLines: NO_PRODUCT_LINES,
+        seeProduct: See_Product,
+        addProducts: Opportunity_Products,
+        updateProblem: Opportunity_Update_Problem,
+    };
+
     @api recordId;
     @track products;
     @track ifProducts = false;  
+    @track ifNegative = false;
     @track error = {};
    
 
     basecolumns = [
-        { label: 'Nom du Produit', fieldName: 'productName', type: 'text' },
-        { label: 'Quantité', fieldName: 'Quantity', type: 'Number' , cellAttributes: {
+        { label:this.labels.productName, fieldName: 'productName', type: 'text' },
+        { label: this.labels.quantity, fieldName: 'Quantity', type: 'Number' , cellAttributes: {
            class: { fieldName: 'quantityColor' } // Applique une classe basée sur la donnée
         } },
-        { label: 'Prix Unitaire', fieldName: 'UnitPrice', type: 'Currency' },
-        { label: 'Prix Total', fieldName: 'TotalPrice', type: 'Currency' },
-        { label: 'Quantité en Stock', fieldName: 'quantityInStock', type: 'Number' },
+        { label:this.labels.unitPrice, fieldName: 'UnitPrice', type: 'Currency' },
+        { label: this.labels.totalPrice, fieldName: 'TotalPrice', type: 'Currency' },
+        { label: this.labels.quantityInStock, fieldName: 'quantityInStock', type: 'Number' },
         {
-            label: 'Supprimer',
+            label: this.labels.delete,
             type: 'button-icon',
             typeAttributes: {
                 iconName: 'utility:delete',
                 name: 'delete_product',
-                title: 'Supprimer',
+                title: this.labels.delete,
                 variant: 'border-filled',
                 iconPosition: 'left',
-                label: 'Supprimer',
+                label: this.labels.delete,
             }
         }
     ];
 
     columnsAdmin = [
-        { label: 'Voir Produit',type: 'button'  ,typeAttributes: {
-            label: 'Voir Produit',
+        { label: this.labels.seeProduct,type: 'button'  ,typeAttributes: {
+            label:this.labels.seeProduct,
             name: 'view_product',
             iconName: 'utility:preview',
-            title: 'Voir Produit',
+            title: this.labels.seeProduct,
             variant: 'brand'
             } }
     ];
@@ -100,17 +127,15 @@ export default class OpportunityProductViewer extends NavigationMixin(LightningE
        
 
         if (data) {
-            console.log('les données ',data);
             
             this.products = data.map(item => ({
                 ...item,
                 productName: item.Product2.Name, // Ajouter un champ à plat
                 quantityInStock: item.Product2.QuantityInStock__c, // Ajouter un champ à plat
-                
-                quantityColor: item.Quantity < item.Product2.QuantityInStock__c  ? 'slds-text-color_error':'slds-text-color_success'
+                quantityColor: item.Quantity < item.Product2.QuantityInStock__c  ? 'slds-text-color_error':'slds-text-color_success',
 
                 }));
-             //   console.log('quantite mois quantie en stock ',(item.Quantity-item.Product2.QuantityInStock__c));
+            this.ifNegative = this.products.some(product => product.Quantity < product.Product2.QuantityInStock__c);
             this.ifProducts =data.length>0;
             this.error=null;
         } else if (error) {
